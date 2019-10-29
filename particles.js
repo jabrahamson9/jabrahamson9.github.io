0424b3de2676d7b0e911a80b3aebc98c7072e553
jabrahamson9 <jcabrahamson9@gmail.com>
2019-10-29 13:29:01 +0100

    everything but honors

--- particles.js
+++ particles.js
@@ -109,7 +109,7 @@ var pJS = function(tag_id, params){
         },
         bubble:{
           distance: 200,
-          size: 80,
+          size: 90,
           duration: 0.4
         },
         repulse:{
